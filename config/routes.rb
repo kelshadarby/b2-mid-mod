f8bdd3a803ef262740758d179b6b21c516a9e533
Kelsha_Darby <ohanaipo@gmail.com>
2020-04-03 11:58:25 -0600

    Add ride mechanic creation route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/mechanics', to: 'mechanics#index'
   get '/mechanics/:mechanic_id', to: 'mechanics#show'
+  post '/mechanics/:mechanic_id', to: 'mechanics#create'
 
   get '/amusement_parks/:amusement_park_id', to: 'amusement_parks#show'
 end
