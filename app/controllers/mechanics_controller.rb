dd39bd457ce4b226d27e6445c5eacb1beeb04ddb
Kelsha_Darby <ohanaipo@gmail.com>
2020-04-03 11:58:52 -0600

    Add create method to mechanics controller

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -6,4 +6,10 @@ class MechanicsController < ApplicationController
   def show
     @mechanic = Mechanic.find(params[:mechanic_id])
   end
+
+  def create
+    @mechanic = Mechanic.find(params[:mechanic_id])
+    ride = Ride.find(params[:ride_id])
+    MechanicRide.create(ride: ride, mechanic: @mechanic)
+  end
 end
